b5afa75435ea313aac74554bbdf08c6d6b34c4b9
Nicolas <nicolas.six@insa-lyon.fr>
2017-03-13 17:30:41 +0100

    move IR instructions and update CMakeLists.txt

---
+++ IR/basicBlock/BasicBlock.cpp
@@ -0,0 +1,6 @@
+#include "BasicBlock.h"
+
+BasicBlock::BasicBlock()
+{
+
+}
---
+++ IR/basicBlock/BasicBlock.h
@@ -0,0 +1,11 @@
+#ifndef BASICBLOCK_H
+#define BASICBLOCK_H
+
+
+class BasicBlock
+{
+public:
+    BasicBlock();
+};
+
+#endif // BASICBLOCK_H
