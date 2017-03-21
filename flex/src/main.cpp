354a2689e61e098f1693f5940da31c1c1b6cb2e8
fteppe <teppe.florent@hotmail.fr>
2017-03-21 08:13:34 +0100

    commit main

--- flex/src/main.cpp
+++ flex/src/main.cpp
@@ -4,13 +4,11 @@
 using namespace std;
 extern int yylex();
 
-void yyerror(int * param, const char * msg)
-{
+void yyerror(int * param, const char * msg) {
 	cout << msg << endl;
 }
 
-int main(void)
-{
+int main(void) {
 	int i = 0;
 	yyparse(&i);
 	cout << i << endl;
