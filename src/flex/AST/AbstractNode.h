f741f7ed257b8c218f7902d25dba4221be99044a
frobion <francois.robion@insa-lyon.fr>
2017-03-23 14:48:59 +0100

    suppress TypeValue from ANode.h

--- src/flex/AST/AbstractNode.h
+++ src/flex/AST/AbstractNode.h
@@ -9,13 +9,6 @@
 
 namespace AST{
 
-    union TypeValue {
-        char character;
-        int int32;
-        long int int64;
-    };
-    typedef union TypeValue TypeValue;
-
     class AbstractNode
     {
     public:
@@ -31,10 +24,17 @@ namespace AST{
          */
         virtual Value evaluate() const = 0;
 
+        /**
+         * @brief printTree Print the tree from this node, with a number a tabulation equal to tabulationNumber
+         * @param tabulationNumber The number of tabulation before each line of the tree of this node
+         */
         virtual void printTree(int tabulationNumber) const;
 
-        // TODO : create class CFG and replace comment below.
-        virtual void buildIR(/*std::shared_ptr<CFG>*/) const = 0;
+        // TODO : replace comment below.
+        /**
+         * @brief buildIR build the IR, and put the correspondant instructions in the provided basic block
+         */
+        virtual void buildIR(/*std::shared_ptr<IR::BasicBloc>*/) const = 0;
 
     protected:
         Value value;
--- src/flex/AST/VariableDeclaration.h
+++ src/flex/AST/VariableDeclaration.h
@@ -1,8 +1,10 @@
 #ifndef VARIABLEDECLARATION_H
 #define VARIABLEDECLARATION_H
 
+#include "AbstractNode.h"
+
 namespace AST {
-    class VariableDeclaration
+    class VariableDeclaration : public AbstractNode
     {
     public:
         VariableDeclaration();
