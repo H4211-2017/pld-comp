9059a4f3e34cf9d8151b411fe024968bb758bc32
Simon Belletier <sbelletier@if501-208-03.insa-lyon.fr>
2017-03-23 11:38:46 +0100

    Begin work on variable scopes

---
+++ src/flex/AST/VariableScope.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+#include "VariableScope.h"
+
+using namespace AST;
+
+
+VariableScope::VariableScope() : 
+	mother(nullptr)
+{
+}
+
+VariableScope::VariableScope( VariableScope & variableScope ) :
+	mother( &variableScope )
+{
+	
+}
+
+void VariableScope::declareVariable( std::string identifiant, std::pair<Type, TypeValue> variable)
+{
+	scope.emplace(identifiant, variable);
+}
+
+std::pair<Type, TypeValue> VariableScope::findVariable( std::string identifiant )
+{
+	try
+	{
+		auto it = scope.find(identifiant);
+		if( it == scope.end() )
+		{
+			if( mother == nullptr )
+			{
+				throw UndeclaredIdException();
+			}
+			else
+			{
+				return mother->findVariable(identifiant);
+			}
+		}
+		else
+		{			
+			return it->second;
+		}
+		
+		
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << "VariableScope::findVariable ( " << identifiant << " ) : "<< e.what() << std::endl;
+		exit(-1);
+	}
+	
+}
---
+++ src/flex/AST/VariableScope.h
@@ -0,0 +1,54 @@
+#ifndef VARIABLESCOPE
+#define VARIABLESCOPE
+
+#include <memory>
+#include <map>
+#include <string>
+#include <exception>
+
+
+#include "AbstractNode.h"
+
+namespace AST{
+
+	class UndeclaredIdException: public std::exception
+	{
+	  virtual const char* what() const throw()
+	  {
+		return "Error : Undeclared Identifier";
+	  }
+		
+	};
+
+
+	class VariableScope
+	{
+	public:
+		VariableScope();
+		
+		VariableScope( VariableScope & );
+		
+		VariableScope( const VariableScope & variableScope); // declared but not defined
+		
+		virtual ~VariableScope();
+		
+		void declareVariable( std::string identifiant, std::pair<Type, TypeValue> variable);
+		
+		std::pair<Type, TypeValue> findVariable( std::string identifiant );
+		
+		
+		
+	protected:
+		//tree hierarchy
+		std::shared_ptr<VariableScope> mother;
+		
+		// map<id, pair<type, valeur>>
+		std::map< std::string, std::pair<Type, TypeValue> > scope;
+		
+		
+	};
+
+
+}
+
+#endif // VARIABLESCOPE
