636c64e1e5f2f0560870449679c801a2df9c63f1
Simon Belletier <sbelletier@if501-208-03.insa-lyon.fr>
2017-03-23 13:58:34 +0100

    fix compilation w/ VariableScope

--- src/flex/AST/VariableScope.h
+++ src/flex/AST/VariableScope.h
@@ -1,54 +1,54 @@
-//#ifndef VARIABLESCOPE
-//#define VARIABLESCOPE
+#ifndef VARIABLESCOPE
+#define VARIABLESCOPE
 
-//#include <memory>
-//#include <map>
-//#include <string>
-//#include <exception>
-
-
-//#include "AbstractNode.h"
-
-//namespace AST{
-
-//	class UndeclaredIdException: public std::exception
-//	{
-//	  virtual const char* what() const throw()
-//	  {
-//		return "Error : Undeclared Identifier";
-//	  }
-		
-//	};
+#include <memory>
+#include <map>
+#include <string>
+#include <exception>
 
 
-//	class VariableScope
-//	{
-//	public:
-//		VariableScope();
+#include "AbstractNode.h"
+
+namespace AST{
+
+	class UndeclaredIdException: public std::exception
+	{
+	  virtual const char* what() const throw()
+	  {
+		return "Error : Undeclared Identifier";
+	  }
 		
-//		VariableScope( VariableScope & );
-		
-//		VariableScope( const VariableScope & variableScope); // declared but not defined
-		
-//		virtual ~VariableScope();
-		
-//		void declareVariable( std::string identifiant, std::pair<Type, TypeValue> variable);
-		
-//		std::pair<Type, TypeValue> findVariable( std::string identifiant );
-		
-		
-		
-//	protected:
-//		//tree hierarchy
-//		std::shared_ptr<VariableScope> mother;
-		
-//		// map<id, pair<type, valeur>>
-//		std::map< std::string, std::pair<Type, TypeValue> > scope;
-		
-		
-//	};
+	};
 
 
-//}
+	class VariableScope
+	{
+	public:
+		VariableScope();
+		
+		VariableScope( VariableScope & );
+		
+		VariableScope( const VariableScope & variableScope); // declared but not defined
+		
+		virtual ~VariableScope();
+		
+		void declareVariable( std::string identifiant, std::pair<Type, TypeValue> variable);
+		
+		std::pair<Type, TypeValue> findVariable( std::string identifiant );
+		
+		
+		
+	protected:
+		//tree hierarchy
+		std::shared_ptr<VariableScope> mother;
+		
+		// map<id, pair<type, valeur>>
+		std::map< std::string, std::pair<Type, TypeValue> > scope;
+		
+		
+	};
 
-//#endif // VARIABLESCOPE
+
+}
+
+#endif // VARIABLESCOPE
