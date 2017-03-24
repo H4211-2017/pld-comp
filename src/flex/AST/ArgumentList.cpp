61249bd1b205fca1b6bb2945b8dc736044a2f9b5
frobion <francois.robion@insa-lyon.fr>
2017-03-24 11:30:16 +0100

    progress on ArgumentList

--- src/flex/AST/ArgumentList.cpp
+++ src/flex/AST/ArgumentList.cpp
@@ -2,8 +2,29 @@
 
 using namespace AST;
 
-ArgumentList::ArgumentList()
+ArgumentList::ArgumentList(VariableSignature variableSignature)
     : AbstractNode("ArgumentList")
+{
+    variableSignatureList.push_back(variableSignature);
+}
+
+ArgumentList::ArgumentList(VariableSignature variableSignature, ArgumentList argumentList)
+    : AbstractNode("ArgumentList"), variableSignatureList(argumentList.variableSignatureList)
+{
+    variableSignatureList.push_back(variableSignature);
+}
+
+void ArgumentList::printTree(int tabulationNumber) const
+{
+
+}
+
+Value ArgumentList::evaluate() const
+{
+
+}
+
+void ArgumentList::buildIR() const
 {
 
 }
--- src/flex/AST/ArgumentList.h
+++ src/flex/AST/ArgumentList.h
@@ -1,13 +1,24 @@
 #ifndef ARGUMENTLIST_H
 #define ARGUMENTLIST_H
 
+#include <vector>
+
 #include "AbstractNode.h"
+#include "VariableSignature.h"
 
 namespace AST {
     class ArgumentList : public AbstractNode
     {
     public:
-        ArgumentList();
+        ArgumentList(VariableSignature variableSignature);
+        ArgumentList(VariableSignature variableSignature, ArgumentList argumentList);
+
+        virtual void printTree(int tabulationNumber) const;
+        virtual Value evaluate() const;
+        virtual void buildIR() const;
+
+    private:
+        std::vector<VariableSignature> variableSignatureList;
     };
 }
 
