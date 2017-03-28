71dbe8f709272bfdfc2ca462590eb4b19898de19
Simon Belletier <sbelletier@if501-208-01.insa-lyon.fr>
2017-03-28 16:51:57 +0200

    Monoline comment & preprocessors are now ignored

--- test/testAST/Preproc.c
+++ test/testAST/Preproc.c
@@ -1,8 +1,15 @@
-#
-#
-#
+#include <somestdlib>
+#include "SomeLib.h"
+#define SOMESTUFF
 
 int32_t main()
 {
-
+//monoline commentary
+/*multiline commentary
+on
+several lines*/
+int32_t bar;
+/* // some twisted
+things*/
+int32_t a;
 }
