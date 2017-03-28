d982c419a54ad7602d34febbb831fa757d4ec96c
Simon Belletier <sbelletier@if501-208-01.insa-lyon.fr>
2017-03-28 10:31:15 +0200

    nettoyage + expressions unaires

--- src/flex/AST/includeCommun.h
+++ src/flex/AST/includeCommun.h
@@ -1,27 +1,34 @@
 #ifndef INCLUDE_COMMUN_H
 #define INCLUDE_COMMUN_H
 
-#include "Constant.h"
-#include "AbstractExpression.h"
-#include "Instruction/ComposedInstruction.h"
-#include "AddExpression.h"
+
 #include "AbstractNode.h"
 #include "Block.h"
-#include "Instruction/UnitInstruction.h"
 #include "Scope.h"
-#include "Instruction/SequenceInstruction.h"
-#include "Variable/VariableSignature.h"
 #include "Value.h"
+
+#include "Expression/Constant.h"
+#include "Expression/AbstractExpression.h"
+#include "Expression/UnaryExpression.h"
+#include "Expression/AddExpression.h"
+#include "Expression/IDExpression.h"
+#include "Expression/AffectationExpression.h"
+
+#include "Instruction/ComposedInstruction.h"
+#include "Instruction/UnitInstruction.h"
+#include "Instruction/SequenceInstruction.h"
+
+#include "Variable/VariableSignature.h"
 #include "Variable/VariableDeclaration.h"
-#include "IDExpression.h"
-#include "AffectationExpression.h"
+#include "Variable/Variable.h"
+
 #include "Function/FunctionSignature.h"
 #include "Function/LArguments.h"
 #include "Function/Function.h"
 #include "Function/FunctionDeclaration.h"
-#include "Variable/Variable.h"
 #include "Function/LParametres.h"
 #include "Function/FunctionCall.h"
+
 #include "Structure/AbstractStructure.h"
 #include "Structure/IfStructure.h"
 #include "Structure/ElseStructure.h"
