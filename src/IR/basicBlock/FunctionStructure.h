4b5c524c4901e31b9b773ef50dd66944040caa0b
Nicolas <nicolas.six@insa-lyon.fr>
2017-04-04 11:06:48 +0200

    add asm generation into FunctionStructure

--- src/IR/basicBlock/FunctionStructure.h
+++ src/IR/basicBlock/FunctionStructure.h
@@ -2,8 +2,10 @@
 #define FUNCTIONSTRUCTURE_H
 
 #include <list>
+#include <ostream>
 
 #include "../function/FunctionBlock.h"
+#include "../config/Enums.h"
 
 namespace IR {
     class FunctionStructure
@@ -12,10 +14,13 @@ namespace IR {
         FunctionStructure();
 
         void addFunction(std::shared_ptr<FunctionBlock> function);
-
         std::list<std::shared_ptr<FunctionBlock> > getFuntionList() const;
 
+        void printASM(std::ostream &os, AsmType asmType);
+
     private:
+        void printX86prolog(std::ostream &os);
+
         std::list<std::shared_ptr<FunctionBlock>> funtionList;
     };
 }
