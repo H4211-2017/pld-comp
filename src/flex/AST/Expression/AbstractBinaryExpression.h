a3c78649054ebfc325728e629645c944d5055f82
Pyhro36 <pierre-louis.lefebvre@insa-lyon.fr>
2017-04-05 17:37:23 +0200

    build IR return instruction

--- src/flex/AST/Expression/AbstractBinaryExpression.h
+++ src/flex/AST/Expression/AbstractBinaryExpression.h
@@ -10,31 +10,33 @@
 #include "AbstractExpression.h"
 
 
-namespace AST{
-
-class AbstractBinaryExpression : public AbstractExpression{
-public:
-    AbstractBinaryExpression(std::string name, std::shared_ptr<AbstractExpression> leftMember,  std::shared_ptr<AbstractExpression> rightMember);
-	virtual ~AbstractBinaryExpression();
-
-    /**
-     * @brief printTree Prints the structure of the expression
-     * @param tabulationNumber the tabulation of this current node.
-     */
-    virtual void printTree(int tabulationNumber) const;
-
-    /**
-     * @brief evaluate pure virtual function
-     * @return the "Value" of the expression, mainly it's type.
-     */
-    virtual Value evaluate() const =0;
-    virtual IR::sh_Memory buildIR(IR::sh_BasicBlock & currentBasicBlock) const =0;
+namespace AST
+{
+	class AbstractBinaryExpression : public AbstractExpression
+	{
 	
-protected:
-    virtual void printOperator() const =0;
+	public:
+		AbstractBinaryExpression(std::string name, std::shared_ptr<AbstractExpression> leftMember,  std::shared_ptr<AbstractExpression> rightMember);
+		virtual ~AbstractBinaryExpression();
 
-	std::shared_ptr<AbstractExpression> rightMember;
-	std::shared_ptr<AbstractExpression> leftMember;
-};
+		/**
+		 * @brief printTree Prints the structure of the expression
+		 * @param tabulationNumber the tabulation of this current node.
+		 */
+		virtual void printTree(int tabulationNumber) const;
+
+		/**
+		 * @brief evaluate pure virtual function
+		 * @return the "Value" of the expression, mainly it's type.
+		 */
+		virtual Value evaluate() const =0;
+		virtual IR::sh_Memory buildIR(IR::sh_BasicBlock & currentBasicBlock) const =0;
+		
+	protected:
+		virtual void printOperator() const =0;
+
+		std::shared_ptr<AbstractExpression> rightMember;
+		std::shared_ptr<AbstractExpression> leftMember;
+	};
 }
 #endif /* SRC_FLEX_AST_ABSTRACTBINARYEXPRESSION_H_ */
